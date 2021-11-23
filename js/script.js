da78c1350543c7d91c85a9ab8156db10e5fe0676
Tricky <wangombevictor5@gmail.com>
2021-11-23 23:41:46 +0300

    finalize html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// $(document).ready(function(){
-//     $('.clickable').click(function(){
-//       $(this).children('#product').toggle();
-//       $(this).children('product-image').toggle();
-//     })
-//   });
 $(document).ready(function(){
 $("p").click(function(){
  alert("is this working");
