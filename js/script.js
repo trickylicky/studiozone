614fb38f5fa78030e7be21e3f8f9c008fccb2154
Tricky <wangombevictor5@gmail.com>
2021-11-24 20:05:07 +0300

    add hover

--- js/script.js
+++ js/script.js
@@ -26,8 +26,35 @@ $(".hide2").click(function(){
     $(".show2").slideToggle();
     $(".hide2").slideToggle();
 });
-$(".middle").hover(function(){
-    $(this).css("background-color", "gray");
-    }, function(){
-    $(this).css("background-color","none");
-  });
+function submit(){
+   var Email = $('#email').val();
+   var Name = $('#name').val();
+   var Message = $('#message').val();
+   if($('#name').val() && $('#email').val()){ 
+       alert(Name+"we have received your message through yoru email"+ Email + "Thank you fro reaching out to us.!")
+}
+};
+$('#work1').hover(function(){
+    $(".road1").toggle();
+});
+$('#work2').hover(function(){
+    $(".road2").toggle();
+});
+$('#work3').hover(function(){
+    $(".road3").toggle();
+});
+$('#work4').hover(function(){
+    $(".road4").toggle();
+});
+$('#work5').hover(function(){
+    $(".road5").toggle();
+});
+$('#work6').hover(function(){
+    $(".road6").toggle();
+});
+$('#work7').hover(function(){
+    $(".road7").toggle();
+});
+$('#work8').hover(function(){
+    $(".road8").toggle();
+});
