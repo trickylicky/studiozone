dd35f3a83d007803214e9967107fa461d361b142
Tricky <wangombevictor5@gmail.com>
2021-11-24 15:23:16 +0300

    perfect toggle

--- js/script.js
+++ js/script.js
@@ -6,16 +6,23 @@ $(".show0").click(function(){
     $(".hide0").slideToggle();
     $(".show0").slideToggle();
 });
-$(".show0").click(function(){
-    $(".hide0").slideToggle();
+$(".hide0").click(function(){
     $(".show0").slideToggle();
-});
-$(".show0").click(function(){
     $(".hide0").slideToggle();
-    $(".show0").slideToggle();
 });
-$(".show0").click(function(){
-    $(".hide0").slideToggle();
-    $(".show0").slideToggle();
+$(".show1").click(function(){
+    $(".hide1").slideToggle();
+    $(".show1").slideToggle();
 });
-
+$(".hide1").click(function(){
+    $(".show1").slideToggle();
+    $(".hide1").slideToggle();
+});
+$(".show2").click(function(){
+    $(".hide2").slideToggle();
+    $(".show2").slideToggle();
+});
+$(".hide2").click(function(){
+    $(".show2").slideToggle();
+    $(".hide2").slideToggle();
+});
