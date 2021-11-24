32b2b026e24812e6b6c98fe96a4566817b2d0e4f
Tricky <wangombevictor5@gmail.com>
2021-11-24 18:19:16 +0300

    try float

--- js/script.js
+++ js/script.js
@@ -26,14 +26,6 @@ $(".hide2").click(function(){
     $(".show2").slideToggle();
     $(".hide2").slideToggle();
 });
-$(document).ready(function() {
-    $('form#apply').submit(function(answer){
-        answer.preventDefault();
-        var nameInput = $('#name').val();
-        var emailInput = $('#email').val();
-        var textAreaInput = $('#message').val();
-        if($('#name').val() && $('#email').val()){
-        alert(nameInput + ", we have received your message. Thank you for reaching out to us."+ " Your Email address:" + emailInput );
-        }else{
-            alert('enter a valid name and email');
-        }}}
+function myEnterFunction() {
+    document.getElementById("demoz").innerHTML = giraffe;
+  }
